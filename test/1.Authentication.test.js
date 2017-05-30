806da41cae1e69c3fbb1b2b7780dedcb085b861b
AlexKvazos <contact@alexkvazos.com>
2017-05-30 01:59:36 -0500

    Use superagent promises

--- test/1.Authentication.test.js
+++ test/1.Authentication.test.js
@@ -6,9 +6,11 @@ describe('Authentication', function() {
 	describe('#login()', function() {
 
 		it('should return invalid login', function(done) {
+			this.timeout(3000);
 			CubedHost.Authentication.login('invalid@email.com', 'password')
 				.then(function(res) {
 					expect(res).to.be.an('object');
+					expect(res.body.success).to.equal(false);
 					done();
 				});
 		});
