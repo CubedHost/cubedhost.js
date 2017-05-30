ad0120023f8c930a4b341e6eea01f079e465ce0d
AlexKvazos <contact@alexkvazos.com>
2017-05-30 02:13:58 -0500

    Add Authentication.register()

--- test/1.Authentication.test.js
+++ test/1.Authentication.test.js
@@ -1,18 +1,24 @@
 const expect = require('chai').expect;
 const CubedHost = require('../');
 
+let invalidUser = {
+	name: 'Invalid User',
+	email: 'invalid@user.com',
+	password: 'invalid'
+};
+
 describe('Authentication', function() {
+	this.timeout(5000);
 
 	describe('#login()', function() {
 
 		it('should return invalid login', function(done) {
-			this.timeout(3000);
-			CubedHost.Authentication.login('invalid@email.com', 'password')
+			CubedHost.Authentication.login(invalidUser)
 				.then(function(res) {
 					expect(res).to.be.an('object');
 					expect(res.body.success).to.equal(false);
 					done();
-				});
+				}).catch(done);
 		});
 
 	});
