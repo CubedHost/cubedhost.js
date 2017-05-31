17ade0ef78e9870194d842e5cf940a7eeb377a31
AlexKvazos <contact@alexkvazos.com>
2017-05-31 01:28:30 -0500

    Add User #forgot()

--- test/1.Authentication.test.js
+++ test/1.Authentication.test.js
@@ -10,7 +10,6 @@ describe('Authentication', function() {
 	this.timeout(5000);
 
 	describe('#login()', function() {
-
 		it('should return valid login', function() {
 			return CubedHost.Authentication.login(user)
 				.then(function(res) {
@@ -19,7 +18,17 @@ describe('Authentication', function() {
 					expect(res.body.success).to.equal(true);
 				});
 		});
+	});
 
+	describe('#forgot()', function() {
+		it('should return forgot password confirmation', function() {
+			return CubedHost.Authentication.forgot('admin+prisma@cubedhost.com')
+				.then(function(res) {
+					expect(res).to.be.an('object');
+					expect(res.status).to.equal(200);
+					expect(res.body.success).to.equal(true);
+				});
+		});
 	});
 
 });
