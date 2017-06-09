db70e5758bc945d99cef74b0173431d0c3fb3a0d
AlexKvazos <contact@alexkvazos.com>
2017-06-08 23:58:21 -0500

    Add Tickets #create() and #getDepartments()

--- test/4.Tickets.test.js
+++ test/4.Tickets.test.js
@@ -41,4 +41,40 @@ describe('Tickets', function() {
 		});
 	});
 
+	describe('#create()', function() {
+		it('should create a new ticket', function() {
+			let ticket = {
+				'subject': 'Automated test ticket',
+				'department': '1',
+				'priority': 'Low',
+				'message': 'Test'
+			};
+
+			return CubedHost.Tickets.create(ticket)
+				.then(function(res) {
+					expect(res).to.be.an('object');
+					expect(res.status).to.equal(200);
+					expect(res.body).to.have.property('id');
+					expect(res.body).to.have.property('tid');
+					expect(res.body.result).to.equal('success');
+				});
+		});
+	});
+
+	describe('#getDepartments()', function() {
+		it('should get ticket departments', function() {
+			return CubedHost.Tickets.getDepartments()
+				.then(function(res) {
+					expect(res).to.be.an('object');
+					expect(res.status).to.equal(200);
+					expect(res.body.success).to.equal(true);
+					expect(res.body.departments).to.be.an('array');
+					expect(res.body.departments[0]).to.have.property('id');
+					expect(res.body.departments[0]).to.have.property('name');
+					expect(res.body.departments[0]).to.not.have.property('awaitingreply');
+					expect(res.body.departments[0]).to.not.have.property('opentickets');
+				});
+		});
+	});
+
 });
